ea2f666c4cbcc8dfe6a058c70b2ace890b74c480
Kijun Sung <kijun93@gmail.com>
2026-01-11 10:25:07 +0900

    add preview view.

--- app/src/main/java/com/kijun/retrica/MainActivity.kt
+++ app/src/main/java/com/kijun/retrica/MainActivity.kt
@@ -4,11 +4,15 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.kijun.retrica.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
@@ -55,7 +59,29 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+        // 수명 주기 동기화 위한 준비.
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+
+            // 프리뷰 객체 초기화 및 바인딩.
+            val preview = Preview.Builder().build().also {
+                it.surfaceProvider = binding.viewPreview.surfaceProvider
+            }
+
+            // 카메라 선택
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // 바인딩되어 있는 use case 해제. 충돌 방지.
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (exception: Exception) {
+                Log.e("CameraX", "Use case binding failed", exception)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
